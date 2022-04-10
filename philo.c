b062eaf8c3f14862a19800ea1b76e9e013a6fdf9
estrong <79113244+Kham02@users.noreply.github.com>
2022-04-11 02:28:16 +0300

    f

--- philo.c
+++ philo.c
@@ -1,6 +1,9 @@
 int	main(int ac, char **av)
 {
-	if (ac < 4)
+	if (ac < 4 || ac > 5)
+	{
+		write(1, "Error\n", 6);
 		return (0);
+	}
 	
 }
--- philo.h
+++ philo.h
@@ -1,4 +1,24 @@
 #ifndef PHILO_H
 #define PHILO_H
 
+typedef struct s_philo
+{
+	pthread_t	t;
+	int			id;
+}t_philo;
+
+typedef struct s_data
+{
+	size_t	time_eat;
+	size_t	time_sleep;
+	size_t	time_die;
+	int		num_philo;
+}t_data;
+
+typedef struct s_forks
+{
+	int	left;
+	int	right;
+}t_forks;
+
 #endif
