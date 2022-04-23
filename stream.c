9f3af09ebae948e6b60b53af80ce28c6fc795ed0
estrong <79113244+Kham02@users.noreply.github.com>
2022-04-23 17:19:07 +0300

    invalid

--- stream.c
+++ stream.c
@@ -22,14 +22,14 @@ void	create_stream(t_data *data, t_philo *philo)
 	time_add(data);
 	while(i < data->num_philo)
 	{
-		pthread_create(&lst->t, NULL, (void *)philo_life, lst);
+		pthread_create(lst->t, NULL, (void *)philo_life, lst);
 		write (1, "st\n", 3);
 		pthread_detach(lst->t);
 		i++;
 		lst = lst->next;
 	}
 	i = 0;
-	pthread_create(&data->th, NULL, (void *)check_die, philo);
+	pthread_create(data->th, NULL, (void *)check_die, philo);
 	pthread_detach(data->th);
 }
 
@@ -43,17 +43,24 @@ void	time_add(t_data *data)
 
 void	philo_life(t_philo *philo)
 {
+	int	i;
+
 	printf("philo->id: %d\n", philo->id);
 	write (1, "lf\n", 3);
 	if ((philo->id % 2) != 0)
 		usleep(philo->data->time_eat / 2);
 	while (philo->data->all_eat_status != 0)
 	{
+		i = philo->data->all_eat_status;
+		if (philo->die_status == 0)
+			break;
+		philo->die_status = -1;
 		pthread_mutex_lock(philo->forks_r);
 		printf("%d, %s\n", philo->id, ": philosopher took the right fork");
 		pthread_mutex_lock(philo->prev->forks_r);
 		printf("%d, %s\n", philo->id, ": philosopher took the left fork");
 		printf("%d, %s\n", philo->id, ": philosopher eat");
+		i--;
 		philo->eat_status++;
 		usleep(philo->data->time_eat);
 		pthread_mutex_unlock(philo->forks_r);
@@ -61,8 +68,8 @@ void	philo_life(t_philo *philo)
 		printf("%d, %s\n", philo->id, ": philosopher sleep");
 		usleep(philo->data->time_sleep);
 		printf("%d, %s\n", philo->id, ": philosopher think");
-		if (philo->die_status == 0)
-			break;
+		if (i == 0)
+			philo->data->all_eat_status--;
 	}
 }
 
